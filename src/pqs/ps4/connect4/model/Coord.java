2ed49b3e6957845e27c1b85386638b9068114f9e
ee08b397 <ee08b397@gmail.com>
2016-04-23 19:17:47 -0400

    check win/lose vert, hort, diag; forgot add Coord

---
+++ src/pqs/ps4/connect4/model/Coord.java
@@ -0,0 +1,31 @@
+package pqs.ps4.connect4.model;
+
+/*
+ * 
+ */
+public class Coord {
+  
+  private final int row;
+  private final int col;
+  
+  public Coord(int row, int col) {
+    if (row >= 0 && row < Config.NUM_ROW && col >= 0 
+        && col < Config.NUM_COL) {
+      
+      this.row = row;
+      this.col = col;  
+    }
+    else {
+      throw new IllegalArgumentException(String.format(
+          "Illegal coordinate row: %d, col: %d. ", row, col));
+    }
+  }
+
+  public int getRow() {
+    return new Integer(this.row);
+  }
+
+  public int getCol() {
+    return new Integer(this.col);
+  }
+}
